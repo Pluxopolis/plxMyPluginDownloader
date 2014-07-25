6ee0b0e67ea178456f23eae7e5acebee3146a5f8
ComputingFroggy <laurent_roche@yahoo.com>
2014-07-25 12:42:40 +0200

    Create en.php
    English language ;-)

---
+++ lang/en.php
@@ -0,0 +1,30 @@
+<?php
+
+$LANG = array(
+
+'L_ERR_CURL_NOT_AVAILABLE'	=> 'cURL function not available',
+'L_ERR_REMOTE_FILE'			=> 'Remote file not reached',
+'L_ERR_DOWNLOAD'			=> 'Error during plugin download',
+'L_ERR_CACHE'				=> 'Can not read data',
+
+'L_ERR_INSTALL'				=> 'Error while downloading and installing the plugin',
+'L_INSTALL_OK'				=> 'Download and install OK',
+
+'L_ERR_CURL'				=> 'Error: cURL function not available.',
+'L_ERR_PLX_PLUGINS'			=> 'Error: Directory '.PLX_PLUGINS.' can not be found',
+'L_ERR_WRITE_ACCESS'		=> 'Error: Directory '.PLX_PLUGINS.' write protected',
+
+'L_ERR_REPOSITORY'			=> 'Error: Can not reach repository',
+
+'L_VERSION'					=> 'version',
+'L_AUTHOR'					=> 'author',
+'L_DOWNLOAD'				=> 'Download',
+'L_UPDATE'					=> 'Update',
+
+
+'L_URL'						=> 'Plugin file Url',
+'L_EXTENSION'				=> '(.zip only)',
+'L_SAVE'					=> 'Save',
+
+);
+?>
